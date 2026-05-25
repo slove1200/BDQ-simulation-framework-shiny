e5b8168f5567e7712260b7993bc688095c2dc273
yujoulin <slove1200@gmail.com>
2026-05-25 10:56:16 +0200

    fix: dur calc and typo

--- app/BDQ_Server/BDQ_Server_MSM.R
+++ app/BDQ_Server/BDQ_Server_MSM.R
@@ -8,16 +8,17 @@ sim_MSM <- function(input, sim_TTPtable, sim_PKtable) {
   
   # calculate duration
   reg1_dur <- (if(input$LD1) input$ldur_1 * ifelse(input$lunit_1 == "2", 1, 1/7) else 0) + 
-  (input$mdur_1 * ifelse(input$munit_1 == "2", 1, 1/7)) +
-  (if(input$MD2_1) input$m2dur_1 * ifelse(input$m2unit_1 == "2", 1, 1/7) else 0)
-
+    (input$mdur_1 * ifelse(input$munit_1 == "2", 1, 1/7)) +
+    (if(input$MD2_1) input$m2dur_1 * ifelse(input$m2unit_1 == "2", 1, 1/7) else 0)
+  
   reg2_dur <- (if(input$LD2) input$ldur_2 * ifelse(input$lunit_2 == "2", 1, 1/7) else 0) + 
-  (if(input$RG2) input$mdur_2 * ifelse(input$munit_2 == "2", 1, 1/7) else 0) +
-  (if(input$MD2_2) input$m2dur_2 * ifelse(input$m2unit_2 == "2", 1, 1/7) else 0)
+    (input$mdur_2 * ifelse(input$munit_2 == "2", 1, 1/7)) +
+    (if(input$MD2_2) input$m2dur_2 * ifelse(input$m2unit_2 == "2", 1, 1/7) else 0)
   
   reg3_dur <- (if(input$LD3) input$ldur_3 * ifelse(input$lunit_3 == "2", 1, 1/7) else 0) + 
-  (if(input$RG3) input$mdur_3 * ifelse(input$munit_3 == "2", 1, 1/7) else 0) +
-  (if(input$MD2_3) input$m2dur_3 * ifelse(input$m2unit_3 == "2", 1, 1/7) else 0)
+    (input$mdur_3 * ifelse(input$munit_3 == "2", 1, 1/7)) +
+    (if(input$MD2_3) input$m2dur_3 * ifelse(input$m2unit_3 == "2", 1, 1/7) else 0)
+  
 
   # Create dataset for simulation
   nsubjects    <- input$nsim
